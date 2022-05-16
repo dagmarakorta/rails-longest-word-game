01ef705daf5119bfa0f0941f3b3632ff5c84bd76
Dagmara Korta <dagmara.korta@gmail.com>
2022-05-16 16:39:35 +0200

    Added some time calculating for scores

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,6 +5,7 @@ class GamesController < ApplicationController
   def new
     alphabet = ("a".."z").to_a
     @letters = []
+    @start_time = Time.now.sec
     10.times do
       @letters << alphabet.sample
     end
@@ -14,10 +15,12 @@ class GamesController < ApplicationController
   def score
     @letters = params[:letters].split('')
     @word = params[:word]
-    @result = run_game(@word, @letters)
+    @start_time = params[:start_time]
+    @end_time = Time.now.sec
+    @result = run_game(@word, @letters, @start_time, @end_time)
   end
 
-  def run_game(attempt, letters)
+  def run_game(attempt, letters, start_time, end_time)
     score = 0
     if existing_word?(attempt) == 'false'
       message = "#{attempt} is not an english word."
@@ -25,7 +28,7 @@ class GamesController < ApplicationController
       message = "Your letters are not in the grid/letters overuse."
     else
       message = "Well done!"
-      score = (attempt.length * 100)
+      score = (attempt.length * 100) - (end_time.to_i - start_time.to_i)
     end
     { message: message, score: score }
   end
